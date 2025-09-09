e8415f3d5550e1aab0cba2dd8150474c6f8c9601
malafisor-arthurloyola <malafisor.es@gmail.com>
2025-09-09 12:39:15 -0300

    Add files via upload

---
+++ BEE 1180.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    
+    int X[N];
+    int menorValor;
+    int posicao;
+    
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &X[i]);
+    }
+    
+    menorValor = X[0];
+    posicao = 0;
+    
+    for (int i = 1; i < N; i++) {
+        if (X[i] < menorValor) {
+            menorValor = X[i];
+            posicao = i;
+        }
+    }
+    
+    printf("Menor valor: %d\n", menorValor);
+    printf("Posicao: %d\n", posicao);
+    
+    return 0;
+}
